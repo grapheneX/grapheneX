ad5e15c0ae9358ec1a2b6a2954c751772f0cc12a
KeyLo99 <keylo99official@gmail.com>
2019-03-29 19:50:49 +0300

    print_header method added to helpers.py and used in main class

--- core/helpers.py
+++ core/helpers.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3.7
 # -*- coding: utf-8 -*-
 
+def print_header():
+    project_desc = ":grapheneX:"
+    print(project_desc)
--- grapheneX.py
+++ grapheneX.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3.7
 # -*- coding: utf-8 -*-
 
+from core.helpers import print_header
+
 def main():
-    print("GrapheneX")
+    print_header()
 
 if __name__ == "__main__":
     main()
