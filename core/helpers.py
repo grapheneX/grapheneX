fb993d25bc3d4469d6765cadc76ab9f9939e4d6c
KeyLo99 <keylo99official@gmail.com>
2019-03-30 21:40:36 +0300

    ASCII logo and project description added

--- core/helpers.py
+++ core/helpers.py
@@ -2,5 +2,16 @@
 # -*- coding: utf-8 -*-
 
 def print_header():
-    project_desc = ":grapheneX:"
+    project_desc = """                          
+           `:sdydmy/.           
+        .+hNNNNNhyNNNdo:        | grapheneX |
+     :sdNNNNNNNNNmsmNNNdys/`    Automated System Hardening Framework
+     dNNNNNNNNNNNNNydmydNNN-    Created for Linux & Windows.
+     dNNNNNNNNNNNNNmohNNNNN-    https://github.com/grapheneX
+     dNNNNNNNNNNNmyhNNNNNNN-    Copyright (C) 2019
+     dNNNNNNNNNNyhNNNNNNNNN-        
+     -odNNNNNNNNmhhhhhhhhs/     
+        ./ymNNNNNNNNNho-        
+            :odNNmy/`                     
+        """
     print(project_desc)
