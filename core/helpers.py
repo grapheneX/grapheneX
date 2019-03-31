279f8452e35c33c98377ded72891379424f1af65
KeyLo99 <keylo99official@gmail.com>
2019-03-31 20:56:26 +0300

    Make project name bold & white on print_header

--- core/helpers.py
+++ core/helpers.py
@@ -5,19 +5,23 @@ import sys
 from core.logcl import GraphenexLogger
 
 def print_header():
+    # Import colorama and termcolor
     def import_colorlibs():
         try:
             global init, colored
             from colorama import init
             from termcolor import colored
+            # use colorama to make termcolor work on Windows
+            init()
         except:
             print("[colorama/termcolor] module not found.\n"+
             "Install requirements.txt with pip.")
             sys.exit()
     import_colorlibs()
+
     project_desc = """                          
            `:sdydmy/.           
-        .+hNNNNNhyNNNdo:        | grapheneX |
+        .+hNNNNNhyNNNdo:        """+colored('| grapheneX |', 'white', attrs=['bold'])+"""
      :sdNNNNNNNNNmsmNNNdys/`    Automated System Hardening Framework
      dNNNNNNNNNNNNNydmydNNN-    Created for Linux & Windows.
      dNNNNNNNNNNNNNmohNNNNN-    https://github.com/grapheneX
