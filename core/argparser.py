02862d1f3fd6281c6fc326865500fab69435eb0b
Emperor  Federico <37400853+emperorfederico@users.noreply.github.com>
2019-04-02 17:19:49 +0300

    argparser.py added

---
+++ core/argparser.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+import argparse
+
+parser = argparse.ArgumentParser(description='GrapheneX Hardening')
+parser.add_argument('-w','--web', help= 'For Web Interface')
+args= parser.parse_args()
+
+#def cli_arg_parser()
