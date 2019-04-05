0142417723ff11d22298f40ce99dedecbcf6a8ae
Emperor  Federico <37400853+emperorfederico@users.noreply.github.com>
2019-04-06 00:15:18 +0300

    Update argparse.py
    Corrected to PEP8 format.

--- core/argparser.py
+++ core/argparser.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 import argparse
 
-parser = argparse.ArgumentParser(description='GrapheneX Hardening')
-parser.add_argument('-w','--web', help= 'For Web Interface')
-args= parser.parse_args()
+def cli_arg_parser():
+  parser = argparse.ArgumentParser(description='GrapheneX Hardening')
+  parser.add_argument('-w',
+                    '--web', 
+                    help='For Web Interface')
+  args= parser.parse_args()
 
-#def cli_arg_parser()
