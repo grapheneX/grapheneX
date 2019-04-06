a422520b0a0305871b303cc575cd8e0b6a90fe79
Furkan Yüksel <ibanez75612@hotmail.com>
2019-04-06 12:39:46 +0300

    editreg.py Added

---
+++ core/win/editreg.py
@@ -0,0 +1,26 @@
+from winreg import *
+
+def change_or_create_reg(valuepath,valuename,subvalue):
+    """
+    valuepath:Regedit Path
+    valuename:Regedit Value Name
+    subvalue:Regedit Sub Value Name
+    """
+    try:
+            key = OpenKey(HKEY_CURRENT_USER, valuepath, 0, KEY_ALL_ACCESS)
+    except:
+            key = CreateKey(HKEY_CURRENT_USER, valuepath)
+    SetValueEx(key, valuename, 0, REG_SZ, subvalue)
+    CloseKey(key)       
+ 
+def delete_reg(valuepath,valuename):
+    """
+    valuepath:Regedit Path
+    valuename:Regedit Value Name
+    """
+    hkey = OpenKey(HKEY_CURRENT_USER, valuepath, 0, KEY_SET_VALUE)
+    try:
+        key = DeleteValue(hkey,valuename)
+        CloseKey(hkey)
+    except FileNotFoundError:
+        print("not fount file path")
