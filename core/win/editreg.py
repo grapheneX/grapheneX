8a7de1bb4ec1683ee958c82800f56d94eb1309c6
KeyLo99 <keylo99official@gmail.com>
2019-04-07 16:14:50 +0300

    Refactor editreg.py

--- core/win/editreg.py
+++ core/win/editreg.py
@@ -3,27 +3,31 @@
 
 from winreg import *
 
-def change_or_create_reg(valuepath,valuename,subvalue):
+def change_or_create_reg(valuepath, valuename, subvalue):
     """
-    valuepath:Regedit Path
-    valuename:Regedit Value Name
-    subvalue:Regedit Sub Value Name
+	Changes / creates registry values using given parameters.
+
+    valuepath: Regedit Path
+    valuename: Regedit Value Name
+    subvalue: Regedit Sub Value Name
     """
+	key = None
     try:
-            key = OpenKey(HKEY_CURRENT_USER, valuepath, 0, KEY_ALL_ACCESS)
+        key = OpenKey(HKEY_CURRENT_USER, valuepath, 0, KEY_ALL_ACCESS)
     except:
-            key = CreateKey(HKEY_CURRENT_USER, valuepath)
+        key = CreateKey(HKEY_CURRENT_USER, valuepath)
     SetValueEx(key, valuename, 0, REG_SZ, subvalue)
     CloseKey(key)       
  
 def delete_reg(valuepath,valuename):
     """
-    valuepath:Regedit Path
-    valuename:Regedit Value Name
+	Removes value from registry.
+
+    valuepath: Regedit Path
+    valuename: Regedit Value Name
     """
     hkey = OpenKey(HKEY_CURRENT_USER, valuepath, 0, KEY_SET_VALUE)
     try:
         key = DeleteValue(hkey,valuename)
         CloseKey(hkey)
-    except FileNotFoundError:
-        print("not fount file path")
+    except: pass
