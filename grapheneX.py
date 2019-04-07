98f6212f288c61da773917c9a1ff9d2493cf1816
KeyLo99 <keylo99official@gmail.com>
2019-04-07 16:09:38 +0300

    Use argparser in main class

--- grapheneX.py
+++ grapheneX.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3.7
 # -*- coding: utf-8 -*-
 
+from core.utils.argparser import parse_cli_args
 from core.utils.helpers import print_header
 
 def main():
+    parse_cli_args()
     print_header()
 
 if __name__ == "__main__":
