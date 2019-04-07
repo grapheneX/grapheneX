4dcb55fb7c6b8c3b90f422bee20e3483d8aa303a
KeyLo99 <keylo99official@gmail.com>
2019-04-07 15:55:32 +0300

    Refactor both exec.py

--- core/linux/exec.py
+++ core/linux/exec.py
@@ -6,7 +6,7 @@ import subprocess
 
 def run_cmd(cmd, **kwargs):
     """
-    (LINUX) Executes command and return it's output.
+    (LINUX) Executes command and returns it's output in UTF-8 format.
     Supports passing `kwargs`.
     """
     result = subprocess.run(shlex.split(cmd), stdout=subprocess.PIPE, **kwargs)
--- core/win/exec.py
+++ core/win/exec.py
@@ -5,5 +5,8 @@ import shlex
 import subprocess
 
 def run_cmd(cmd, shell=True):
+    """
+    (WINDOWS) Executes command and returns it's output in UTF-8 format.
+    """
     result = subprocess.run(shlex.split(cmd), stdout=subprocess.PIPE, shell=shell)
     return result.stdout.decode('utf-8','replace')
