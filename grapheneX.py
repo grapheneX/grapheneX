d47b7b48c4bd0670211a0c0b164d07c86a2da579
Efe Aydın <3feaydin@gmail.com>
2019-04-08 22:05:19 +0300

    imported shell in main file

--- grapheneX.py
+++ grapheneX.py
@@ -3,10 +3,14 @@
 
 from core.utils.argparser import parse_cli_args
 from core.utils.helpers import print_header
+from core.shell import Shell
 
 def main():
     parse_cli_args()
     print_header()
+
+    shell = Shell()
+    shell.cmdloop()
 
 if __name__ == "__main__":
     main()
