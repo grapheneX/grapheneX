0cebbc606ac1c758627073e73a02e69b24087847
KeyLo99 <keylo99official@gmail.com>
2019-04-17 14:08:54 +0300

    + newline -> core/hrd/linux/kernel.py

--- core/hrd/linux/kernel.py
+++ core/hrd/linux/kernel.py
@@ -6,3 +6,4 @@ class Logs_Restrict_Access():
         """Restricting access to kernel logs."""
 
         return """echo "kernel.dmesg_restrict = 1" > /etc/sysctl.d/50-dmesg-restrict.conf"""
+        
