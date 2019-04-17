4070f275f7a77f03d1fa7374277d31d2c8172313
KeyLo99 <keylo99official@gmail.com>
2019-04-17 14:29:43 +0300

    New hardening method added for Windows - example
    Disable RDP

--- core/hrd/win/firewall.py
+++ core/hrd/win/firewall.py
@@ -6,4 +6,9 @@ class Disable_File_Sharing:
         """Disable File and Printer Sharing."""
 
         return """netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=No"""
-        
+
+class Disable_RDP:
+    def command(self):
+        """Disable Remote Desktop Protocol."""
+
+        return """reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f"""        
