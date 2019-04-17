8ab820c01c5bc7aa7e7c75a9aa778ed22aa1889b
KeyLo99 <keylo99official@gmail.com>
2019-04-17 14:13:15 +0300

    Example hardening method for Windows added
    Disable File and Printer sharing

--- core/hrd/win/firewall.py
+++ core/hrd/win/firewall.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3.7
 # -*- coding: utf-8 -*-
+
+class Disable_File_Sharing:
+    def command(self):
+        """Disable File and Printer Sharing."""
+
+        return """netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=No"""
+        
