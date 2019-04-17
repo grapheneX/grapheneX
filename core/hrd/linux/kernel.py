5b1ba67f7815386b869e21c7bdb3cb5d897f70c8
KeyLo99 <keylo99official@gmail.com>
2019-04-17 14:18:08 +0300

    New hardening method for Linux kernel
    Restrict access to kernel logs

--- core/hrd/linux/kernel.py
+++ core/hrd/linux/kernel.py
@@ -6,4 +6,10 @@ class Logs_Restrict_Access:
         """Restricting access to kernel logs."""
 
         return """echo "kernel.dmesg_restrict = 1" > /etc/sysctl.d/50-dmesg-restrict.conf"""
+
+class Pointers_Restrict_Access:
+    def command(self):
+        """Restricting access to kernel pointers."""
+
+        return """echo "kernel.kptr_restrict = 1" > /etc/sysctl.d/50-kptr-restrict.conf"""
         
