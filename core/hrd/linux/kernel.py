3d39b21e8e339e7bc2cb42b2493e4658515249d8
KeyLo99 <keylo99official@gmail.com>
2019-04-17 13:27:58 +0300

    Add self parameter to command() method

--- core/hrd/linux/kernel.py
+++ core/hrd/linux/kernel.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 class Logs_Restrict_Access():
-    def command():
+    def command(self):
         """Restricting access to kernel logs."""
 
         return """echo "kernel.dmesg_restrict = 1" > /etc/sysctl.d/50-dmesg-restrict.conf"""
