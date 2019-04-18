dab895bfd85ce905373aba6fb94fc8d3c50e1df1
KeyLo99 <keylo99official@gmail.com>
2019-04-18 21:23:42 +0300

    Change log message for help method exception

--- core/cli/help.py
+++ core/cli/help.py
@@ -21,8 +21,7 @@ class Help:
                     doc = func.__doc__ if func.__doc__ else "No description"
                     print(f"\n{func.__name__[3:]} description:\n{30*'='}\n{doc}\n")
             except AttributeError:
-                print('Command not found!')
-
+                print(f"Cannot find help method for \"{arg}\".")
         else:   
             # Create table for all commands
             table_data = [['Command', 'Description']]
