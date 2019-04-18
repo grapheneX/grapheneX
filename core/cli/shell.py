6d462ad41efcd256745dd56f3ebf81327ac25cbd
KeyLo99 <keylo99official@gmail.com>
2019-04-18 21:35:02 +0300

    Refactor shell.py

--- core/cli/shell.py
+++ core/cli/shell.py
@@ -1,11 +1,11 @@
 from cmd import Cmd
-
 from core.cli.commands import ShellCommands
 
 
 class Shell(ShellCommands, Cmd):
-    harden_str = ""  # this attribute is current harden module string
-    intro = "Welcome Graphenex interactive shell. Type help or ? to list commands."
+    # Current harden module string
+    harden_str = ""
+    intro = "Welcome to the grapheneX interactive shell. Type \"help\" or \"?\" to list commands."
     prompt = f"[gX]> "
 
     @property
