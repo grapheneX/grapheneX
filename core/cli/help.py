4425d53dea66acc53dfd5414682042ed0ba9f1da
KeyLo99 <keylo99official@gmail.com>
2019-04-18 21:18:15 +0300

    Change doc of do_help method

--- core/cli/help.py
+++ core/cli/help.py
@@ -7,7 +7,7 @@ class Help:
     """
 
     def do_help(self, arg):
-        'List available commands with "help" or detailed help with "help cmd".'
+        'List available commands with "help" or show detailed help with "help <cmd>".'
         if arg:
             try:
                 func = getattr(self, f"do_{arg}")
