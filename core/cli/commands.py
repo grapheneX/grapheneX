7a098a1b9d4666082cc17524a7093eef739a803e
KeyLo99 <keylo99official@gmail.com>
2019-04-18 22:05:42 +0300

    Show random farewell message on exit

--- core/cli/commands.py
+++ core/cli/commands.py
@@ -1,6 +1,7 @@
 from core.utils.logcl import GraphenexLogger
 from core.cli.help import Help
 from core.utils.helpers import check_os
+import random
 import os
 
 logger = GraphenexLogger(__name__)
@@ -16,8 +17,20 @@ class ShellCommands(Help):
     def do_exit(self, arg):
         "Exit interactive shell"
 
-        logger.info('Exit shell')
-        print("Bye")
+        exit_msgs = [
+            "Bye!",
+            "Hope to see you soon!",
+            "Take care!",
+            "I am not going to miss you!",
+            "Gonna miss you!",
+            "Thank God, you're going. What a relief!",
+            "Fare thee well!",
+            "Farewell, boss.", 
+            "Daha karpuz kesecektik.",
+            "Bon voyage!",
+            "Regards."
+            "Exiting..."]
+        logger.info(random.choice(exit_msgs))
         return True
 
     def do_clear(self, arg):
