260b655a28d63543af81dc4bdab103e06e4b1b3b
Efe Aydın <3feaydin@gmail.com>
2019-04-22 10:17:14 +0300

    search command added

--- core/cli/commands.py
+++ core/cli/commands.py
@@ -4,6 +4,9 @@
 from core.utils.logcl import GraphenexLogger
 from core.cli.help import Help
 from core.utils.helpers import check_os
+from pprint import pprint
+import importlib.util
+import inspect
 import random
 import os
 
@@ -43,6 +46,41 @@ class ShellCommands(Help):
         """Clear terminal"""
 
         os.system("cls" if check_os() else "clear")
+
+    def do_search(self, arg):
+        """Search for modules"""
+        hrd_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                '..', 'hrd')
+
+        hrd_os = 'win' if check_os() else 'linux'
+        files = [os.path.join(hrd_dir, hrd_os, f) for f in os.listdir(os.path.join(hrd_dir, hrd_os)) if f.endswith('.py')]
+
+        modules = dict()
+        for path in files:
+            module_name = os.path.basename(path)[:-3]
+            spec = importlib.util.spec_from_file_location(module_name, path)
+            hrd = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(hrd)
+            modules[module_name] = {}
+            for name, obj in inspect.getmembers(hrd, inspect.isclass):
+                modules[module_name][name] = obj
+
+        if not arg:
+            for k, v in modules.items():
+                print(k)
+                for name, module in v.items():
+                    print(' +--', name, '-->', inspect.getdoc(module.command))
+        else:
+            if arg in modules.keys():
+                print(arg)
+                for name, module in modules[arg].items():
+                    print(' +--', name, '-->', inspect.getdoc(module.command))
+            else:
+                for k, v in modules.items():
+                    for name, module in v.items():
+                        if arg.lower() in name.lower():
+                            print(name, '-->', inspect.getdoc(module.command))
+
 
     def default(self, line):
         logger.error("Command not found.")
