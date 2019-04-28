540e4d7a85e4e8137226dcc30cc80e4861468e8c
Enes Okutan <enes.okutan@hotmail.com>
2019-04-28 19:17:52 +0000

    fixed the problem

--- core/cli/commands.py
+++ core/cli/commands.py
@@ -18,7 +18,8 @@ class ShellCommands(Help):
         """Switch between modules or namespaces"""
 
         if arg:
-            if arg.lower() in self.modules.keys():
+            arg = arg.lower()
+            if arg in self.modules.keys():
                 logger.info(f"Switched to \"{arg}\" namespace." +
                             " Use 'list' to see available modules.")
                 self.namespace = arg
