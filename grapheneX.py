830491218977e850d203061a387ba5924022f170
Efe Aydın <3feaydin@gmail.com>
2019-04-29 21:36:48 +0300

    root check updated

--- grapheneX.py
+++ grapheneX.py
@@ -2,20 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from core.utils.argparser import parse_cli_args
-from core.utils.helpers import print_header, is_root, check_os
+from core.utils.helpers import print_header, check_privileges
 from core.cli.shell import Shell
 from core.utils.logcl import GraphenexLogger
-
-logger = GraphenexLogger(__name__)
 
 def main():
     parse_cli_args()
     print_header()
-
-    if not check_os():
-        if not is_root():
-            logger.warn('Some functions won\'t work without root access, try running the script with sudo.')
-
+    check_privileges()
     shell = Shell()
     try:
         shell.cmdloop()
