3f9b603481e96eb39d7587352147d5386cebff58
Enes Okutan <enes.okutan@hotmail.com>
2019-05-09 01:30:17 +0000

    Added web parameter control and web server launching. Also, parsed terminal and web functions

--- grapheneX.py
+++ grapheneX.py
@@ -2,18 +2,28 @@
 # -*- coding: utf-8 -*-
 
 from core.utils.argparser import parse_cli_args
-from core.utils.helpers import print_header, check_privileges
+from core.utils.helpers import print_header, check_privileges, check_host_port,logger
 from core.cli.shell import Shell
+from core.web.server import *
 
 def main():
-    parse_cli_args()
-    print_header()
-    check_privileges()
-    shell = Shell()
-    try:
-        shell.cmdloop()
-    except KeyboardInterrupt:
-        shell.do_EOF(None)
+    args = parse_cli_args()
+    if(args['web']):
+        host, port = parser_host_port(args['host_port'])
+        try:
+            app.run(host=host, port=port)
+        except:
+            logger.error("Invalid Host & Port info")
+            logger.info('Using default (host:0.0.0.0, port:8080)')
+            app.run(host='0.0.0.0', port='8080')
+    else:
+    	print_header()
+    	check_privileges()
+    	shell = Shell()
+    	try:
+    		shell.cmdloop()
+    	except KeyboardInterrupt:
+    		shell.do_EOF(None)
 
 if __name__ == "__main__":
     main()
