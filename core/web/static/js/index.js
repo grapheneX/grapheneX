c11414ca810923fdb7e3b5f9abbfca0073adfae3
easyly471 <easyly@protonmail.com>
2019-05-09 22:00:00 +0300

    front end ~ II

--- core/web/static/js/index.js
+++ core/web/static/js/index.js
@@ -1,1 +1,28 @@
-console.log('loaded');
+//Page is ready
+
+// test code
+var module_names = [
+    Object.create({ name: "Disable_File_Sharing", state: "open" }),
+    Object.create({ name: "Disable_RDP", state: "open" }),
+    Object.create({ name: "Other_Harden_Method", state: "open" })
+]
+// test code 
+
+toggleDiv = (obj) => {
+    $("#" + obj.name + "_drawer").slideToggle("slow");
+    
+    // Todo: Add ajax query
+
+    // test code
+    $("#" + obj.name + "_logs").val("Executing...");
+    // test code
+}
+
+$(function () {
+    module_names.forEach(elem => {
+        $("#" + elem.name + "_btn").click(() => {
+            $("#" + elem.name + "_ico").toggleClass('rotated');
+            toggleDiv(elem);
+        })
+    })
+})
