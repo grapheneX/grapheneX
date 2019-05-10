762490d3cb1015a8ff1fbd57cc76f47b17c726b1
ibanez75612 <ibanez75612@gmail.com>
2019-05-10 14:05:35 +0300

    Socket IO Connection

--- core/web/static/js/index.js
+++ core/web/static/js/index.js
@@ -1,1 +1,27 @@
 console.log('loaded');
+
+$(document).ready(function() {
+    // Connect to the Socket.IO server.
+    // http[s]://<domain>:<port>[/<namespace>]
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+});
+
+function getNsList() {
+    //Empty Function
+    return null;
+}
+
+function getModList(ns) {
+    //Empty Function
+    return null;
+}
+
+function getModInfo(mod) {
+    //Empty Function
+    return null;
+}
+
+function harden(mod) {
+    //Empty Function
+    return null;
+}
