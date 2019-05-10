ad8578499ae430f70114d468721db9665463c203
ibanez75612 <ibanez75612@gmail.com>
2019-05-10 14:12:25 +0300

    added to connection event

--- core/web/static/js/index.js
+++ core/web/static/js/index.js
@@ -4,6 +4,13 @@ $(document).ready(function() {
     // Connect to the Socket.IO server.
     // http[s]://<domain>:<port>[/<namespace>]
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+    // Event handler for new connections.
+    // The callback function is invoked when a connection with the
+    // server is established.
+    socket.on('connect', function() {
+        socket.emit('my_event', {data: 'I\'m connected!'});
+    });
 });
 
 function getNsList() {
