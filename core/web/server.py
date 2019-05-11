6f254b52836a1a0742854c3b63920bc31a98f606
KeyLo99 <keylo99official@gmail.com>
2019-05-11 17:38:14 +0300

    Change title of the project

--- core/web/server.py
+++ core/web/server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('index.html', title="Graphenex Web")
+    return render_template('index.html', title="grapheneX [Web]")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
