49d62885f757201224cb911d3530767ba8c69d87
KeyLo99 <keylo99official@gmail.com>
2019-05-11 18:11:00 +0300

    Fix log messages on server.py

--- core/web/server.py
+++ core/web/server.py
@@ -9,14 +9,14 @@ def main():
     return render_template('index.html', title="grapheneX [Web]")
 
 def run_server(server_params=('0.0.0.0', '8080')):
-    logger.info("Starting server at " + ':'.join(server_params))
+    logger.info("Starting server...")
     try:
         app.run(host=server_params[0], port=server_params[1], debug=False)
     except (PermissionError, ValueError):
         logger.error('Invalid host & port address. Restarting with default host and port.')
         run_server()
     except Exception as e:
-        logger.error('Unable to start server:\n' + str(e))
+        logger.error('Unable to start server: ' + str(e))
         
 
 if __name__ == '__main__':
