8f2cff762cecb552ac8a9a31e2b02391e1ae0781
KeyLo99 <keylo99official@gmail.com>
2019-05-15 17:35:45 +0300

    Refactor the new structure that added with pr38

--- core/web/views.py
+++ core/web/views.py
@@ -1,18 +1,14 @@
 from core.web import app, socketio
 from core.utils.helpers import get_os_info
 from core.utils.logcl import GraphenexLogger
-
 from flask import render_template
 from flask_socketio import emit
 
-
 logger = GraphenexLogger(__name__)
-
 
 @app.route('/')
 def main():
-    return render_template('index.html', title="Graphenex Web", sys_info=get_os_info())
-
+    return render_template('index.html', title="grapheneX [Web]", sys_info=get_os_info())
 
 # Don't use this method
 # We will sending system information in render_template
@@ -20,3 +16,7 @@ def main():
 def request_system_info(data):
     logger.info("Sending system info")
     emit("response system info", get_os_info())
+
+@socketio.on('connected')
+def connected_event(msg):
+    print(msg)
