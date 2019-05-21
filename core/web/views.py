2926e868f04e6bd741ea2d20e5e27a2d76a56575
KeyLo99 <keylo99official@gmail.com>
2019-05-22 00:52:20 +0300

    Reduce the number of log messages

--- core/web/views.py
+++ core/web/views.py
@@ -20,22 +20,18 @@ def main():
 
 @socketio.on('get_namespaces')
 def send_namespaces(data):
-    logger.info('Sending namespace list.')
     emit('get_namespaces', {'namespaces': list(module_dict.keys())})
 
 @socketio.on('get_current_namespace')
 def send_current_namespace(data):
-    logger.info('Sending current namespace.')
     emit('get_current_namespace', {'current_namespace': current_namespace})
 
 @socketio.on('send_current_namespace')
 def get_current_namespace(data):
-    logger.info(f'Current namespace changed: {data}')
-    logger.info(f'Sending modules of {data}')
     modules = list()
     mod_dict = module_dict.get(data)
     if mod_dict == None:
-        logger.warn(f"Non-existent namespace: {data}")
+        logger.warn(f"Non-existent namespace: {data}.")
     else:
         current_namespace = data
         for name, mod in mod_dict.items():
@@ -44,6 +40,7 @@ def get_current_namespace(data):
                 'desc': inspect.getdoc(mod().command),
                 'source': inspect.getsource(mod().command).split("\"\"\"")[-2]
             })
+        logger.info(f'Sending modules of {data}.')
         emit('get_module', modules)
 
 @socketio.on('search_module')
