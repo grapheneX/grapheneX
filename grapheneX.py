7014147efebb00d921b0f2bbd8387fa1df0e6771
bufgix <ooruc471@yandex.com>
2019-06-27 17:35:00 +0300

    --open arg fix

--- grapheneX.py
+++ grapheneX.py
@@ -3,15 +3,20 @@
 
 from core.utils.argparser import parse_cli_args
 from core.utils.helpers import print_header
+from core.utils.logcl import GraphenexLogger
 from core.cli.shell import start_cli
 from core.web import run_server
+
+logger = GraphenexLogger('Graphenex')
 
 def main():
     args = parse_cli_args()
     print_header()
     if(args['web']):
-        run_server(args, open_browser=args['open'])
+        run_server(args)
     else:
+        if args['open']:
+            logger.warn("--open argument is unnecessary. Use with -w or --web")
         start_cli()
 
 if __name__ == "__main__":
