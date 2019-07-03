78d402b18e4ef787201c5e19d3ea4f79ac8cc2fc
bufgix <ooruc471@yandex.com>
2019-07-03 03:44:24 +0300

    prepare pypi ~ II

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author_email="graphenex.project@protonmail.com",
     license="GPLv3",
     classifiers=[
-        "License :: OSI Approved :: GPLv3 License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
