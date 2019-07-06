93911891dbee2737603a106234e4a49086ca88c3
KeyLo99 <keylo99official@gmail.com>
2019-07-06 20:11:30 +0300

    Refactor setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools import setup, find_packages
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text(encoding="utf-8")
 
-
 setup(
     name="graphenex",
     version="1.0.0",
@@ -16,18 +15,25 @@ setup(
     author_email="graphenex.project@protonmail.com",
     license="GPLv3",
     classifiers=[
-        "Operating System :: OS Independent",
-        "Topic :: Security",
-        "Topic :: Utilities",
         "Intended Audience :: Developers",
         "Intended Audience :: End Users/Desktop",
         "Intended Audience :: System Administrators",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Natural Language :: English",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
+        "Topic :: Security",
+        "Topic :: Utilities",
     ],
-    packages=["graphenex", "graphenex.core", "graphenex.core.web", "graphenex.core.cli", "graphenex.core.hrd", "graphenex.core.utils"],
+    packages=[
+        "graphenex", 
+        "graphenex.core", 
+        "graphenex.core.web", 
+        "graphenex.core.cli", 
+        "graphenex.core.hrd", 
+        "graphenex.core.utils"
+    ],
     include_package_data=True,
     install_requires=["flask-socketio", "coloredlogs", "terminaltables", "pyinquirer"],
     entry_points={
